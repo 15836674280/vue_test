bf9979a2673082f055614f501ed92d433f1168f4
good <8208005+a-slice@user.noreply.gitee.com>
2022-01-23 14:49:35 +0800

    home静态页面的初始化

--- src/main.js
+++ src/main.js
@@ -1,8 +1,28 @@
 import Vue from 'vue'
 import App from './App.vue'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+//引入VueRouter
+import VueRouter from 'vue-router'
+//三级联动组件----全局组件
+import TypeNav from '@/pages/Home/TypeNav'
+//第一个参数：全局组件的名字，第二个参数：哪一个组件
+Vue.component(TypeNav.name, TypeNav);
+//引入路由器
+import router from './router'
+//test
+import { reqgetCategoryList } from '@/api'
+reqgetCategoryList();
 
+//关闭Vue的生产提示
 Vue.config.productionTip = false
 
+//应用饿了么UI插件
+Vue.use(ElementUI);
+//应用路由插件
+Vue.use(VueRouter);
+
 new Vue({
-  render: h => h(App),
+    render: h => h(App),
+    router: router,
 }).$mount('#app')
---
+++ src/pages/Home/index.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <!-- 全局的三级菜单 -->
+    <TypeNav></TypeNav>
+    <ListContainer />
+    <Recommend />
+    <Rank />
+    <Like />
+    <!-- Floor这个组件：自己在组件内部是没有发请求的，数据是父组件给的 -->
+    <Floor />
+    <Brand />
+  </div>
+</template>
+
+<script>
+//非路由组件在使用的时候分为三大步：定义、注册、使用
+import ListContainer from "./ListContainer";
+import Recommend from "./Recommend";
+import Rank from "./Rank";
+import Like from "./Like";
+import Brand from "./Brand";
+import Floor from "./Floor";
+export default {
+  name: "",
+  components: {
+    ListContainer,
+    Recommend,
+    Rank,
+    Like,
+    Brand,
+    Floor,
+  },
+  data() {
+    return {};
+  },
+
+  mounted() {},
+
+  methods: {},
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
